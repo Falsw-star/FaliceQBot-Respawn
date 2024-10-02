37e84cee2b71419ede3a293710dc8946dadb29f0
Falsw <Falsestar114514@outlook.com>
2024-10-02 20:40:55 +0800

    FastPoker(不可用) Chronocat-Crycchicat尚未支持MacOS

---
+++ plugins/FastPoker.py
@@ -0,0 +1,30 @@
+from faliceqbot import Plugin, Message
+import time
+
+Poker = Plugin("FastPoker")
+
+def poke(message: Message):
+    args = message.get_args()
+    if not len(args) == 2:
+        message.respond('Usage: /poke <qqid> <count>')
+        return
+    userid = int(args[0])
+    count = int(args[1])
+    if count > 20 and not message.get_permission() == 3:
+        message.respond('Too Much...')
+        return
+    operatorid = message.user.id
+    if message.private:
+        sendmsg = message.Sender.send_private_message
+        cid = message.user.id
+    else:
+        sendmsg = message.Sender.send_message
+        cid = message.group_id
+    for i in range(count):
+        sendmsg(cid, f'<chronocat:poke user-id="{userid}" operator-id="{operatorid}"/>')
+        time.sleep(0.1)
+
+Poker.onCommand(poke, ['poke'])
+
+def load():
+    return Poker
